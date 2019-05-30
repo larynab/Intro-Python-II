721e6dfff578fc6471e484d7ed77d1d0a72a037a
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-05-30 12:50:47 -0700

    got walking around down

--- src/adv.py
+++ src/adv.py
@@ -6,19 +6,13 @@ room = {
     'outside':  Room("Outside Cave Entrance",
                 """North of you, the cave mount beckons"""),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."""),
 }
 
 
@@ -49,6 +43,35 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+#welcome message
+
+#variable
+starter_room = room['outside']
+room = starter_room
 def current_room():
-    print("Welcome, adventurer, you have travelled far and ended up near a cave. You stand outside waiting and looking where to go next")
-    print("You are currently in Room: %s" % (room))
+   print("You are currently in Room: %s \n" % (room))
+
+#main_loop
+while True:
+    if room == starter_room:
+        print("Welcome, adventurer, you have travelled far and ended up near a cave. You stand outside waiting and looking where to go next\n")    
+    #loop print
+    print(f"You are in the: {room.name}. {room.description}. \n")
+    #variable
+    direction = input("please proceed with the directions north > n, south > s, east > e, west > w \n")
+
+    if direction == 'n':
+        print("going north \n")
+        room = room.n_to
+    if direction == 's':
+        print("going south \n")
+        room = room.s_to
+    if direction == 'e':
+        print("going east \n")
+        room = room.e_to
+    if direction == 'w':
+        print("going west \n")
+        room = room.w_to    
+    elif direction == 'q':
+        print("see you later \n")
+        quit
--- src/room.py
+++ src/room.py
@@ -2,10 +2,13 @@
 # description attributes.
 class Room:
     """the enviroments where our characters will traverse and interact with"""
-    def __init__(self, terrain, light, hazards):
+    def __init__(self, name, description, terrain = None, light = None, hazards = None):
+        self.name = name
+        self.description =description
         self.terrain = terrain
         self.light = light
         self.hazards = hazards
+        self.n_to = None
     def ambience(self, music):
         if music >0 and hazards < 1:
             hazards += music
